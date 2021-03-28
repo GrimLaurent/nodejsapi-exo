8a9b208573c34cb3f5207dc109bbe470f978ed7b
Laurent Grimaldi <62988176+GrimLaurent@users.noreply.github.com>
2021-03-28 11:52:53 +0200

    push to server

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
-const db = require("./app/models");
+const db = require("./api/models");
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
