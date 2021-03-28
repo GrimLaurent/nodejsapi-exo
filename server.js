c223a6dc1526d64f8d10d77524e3f2ece6e714f0
Laurent Grimaldi <62988176+GrimLaurent@users.noreply.github.com>
2021-03-28 12:25:21 +0200

    push to server

--- server.js
+++ server.js
@@ -4,9 +4,22 @@ const cors = require("cors");
 
 const app = express();
 
-var corsOptions = {
-  origin: "http://51.75.202.45:8081",
-};
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+//var corsOptions = {
+//  origin: "http://51.75.202.45:8081",
+//};
 
 require("./api/routes/turorial.routes")(app);
 // set port, listen for requests
