0c4ef1e10052a20b28098b7b0706e44d67ccc58b
Laurent Grimaldi <62988176+GrimLaurent@users.noreply.github.com>
2021-03-28 13:16:17 +0200

    push to server

--- server.js
+++ server.js
@@ -1,59 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const http = require("http");
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("Requête reçue !");
-  next();
-});
-
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: "Votre requête a bien été reçue !" });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("Réponse envoyée avec succès !");
-});
-
-//var corsOptions = {
-//  origin: "http://localhost:8080",
-//};
-
-app.use(cors());
-
-app.set("port", process.env.PORT || 3000);
-const server = http.createServer(app);
-
-server.listen(process.env.PORT || 3000);
+var corsOptions = {
+  origin: "http://localhost:8080",
+};
 
 require("./api/routes/turorial.routes")(app);
 // set port, listen for requests
-//const PORT = process.env.PORT || 8080;
-//app.listen(PORT, () => {
-//  console.log(`Server is running on port ${PORT}.`);
-//});
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
 
 const db = require("./api/models");
 db.mongoose
