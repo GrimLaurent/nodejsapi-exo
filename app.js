337408cddcc17d5e00c2b5f0fe0db784f7a9cc78
Laurent Grimaldi <62988176+GrimLaurent@users.noreply.github.com>
2021-04-01 12:22:55 +0200

    CROS error

--- app.js
+++ app.js
@@ -62,7 +62,8 @@ app.use((req, res, next) => {
   //    "Access-Control-Allow-Methods",
   //    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
   //  );
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8081");
+  //res.setHeader("Access-Control-Allow-Origin", "http://localhost:8081");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
